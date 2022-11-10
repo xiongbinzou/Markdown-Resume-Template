10ab8ae7e903a5005389eff8f03bd10243e1a67f
Eleven ZOU <85606371+xiongbinzou@users.noreply.github.com>
2022-11-11 00:13:56 +0800

    Create main.py
    用于jenkins构建测试

---
+++ main.py
@@ -0,0 +1,9 @@
+Class Greet:
+    def __init__(self):
+        self.strs = "Hello world!"
+        
+    def print_string(self):
+        print(self.strs)
+        
+if __name__ == "__main__":
+    Greet().print_string()
